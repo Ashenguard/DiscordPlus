965fc10a45505975923fb79139b0c47f531a25dc
Ashenguard <myminecraft2019@gmail.com>
2021-03-27 17:57:07 +0430

    Commit #6 - Fix the requirements

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setuptools.setup(
     ],
     python_requires='>=3.6',
     include_package_data=True,
-    install_requires=["discord.py", "pyyaml", "flask"],
+    install_requires=["discord.py", "pyyaml", "flask", "requests"],
 )
